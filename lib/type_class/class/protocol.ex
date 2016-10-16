2f76934a0bd029e405fd01e0222f99c4709f4c40
Brooklyn Zelenka <bez@brooklynzelenka.com>
2016-10-16 19:37:51 +0900

    typo

--- lib/type_class/class/protocol.ex
+++ lib/type_class/class/protocol.ex
@@ -9,17 +9,19 @@ defmodule TypeClass.Class.Protocol do
   defmacro run do
     quote do
       class_module = __MODULE__
+      protocol_module = Module.concat([class_module, Protocol])
+
       funs = Module.get_attribute(class_module, :where)
 
       # SO MUCH PLACEHOLDER!
 
-      defmodule Module.concat([class_module, Protocol]) do
+      defmodule protocol_module do
         Enum.each(funs, Protocol.def)
       end
 
       funs
       |> extract_names
-      Enum.each(fn fun_name -> defdelegate(fun, to: class_module) end)
+      Enum.each(fn fun_name -> defdelegate(fun, to: protocol_module) end)
     end
   end
 end
