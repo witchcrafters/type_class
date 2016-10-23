7d6c8f8bf9d9f3446c371f2bba08ad6653f6b289
Brooklyn Zelenka <bez@brooklynzelenka.com>
2016-10-22 23:03:30 -0700

    Very top-level API

--- lib/type_class.ex
+++ lib/type_class.ex
@@ -1,2 +1,8 @@
 defmodule TypeClass do
+
+  defdelegate defclass(name, do: body), to: TypeClass.Class
+  defdelegate definstance(class, for: type, do: body), to: TypeClass.Instance
+
+  defdelegate defproperty(fun_head, do: body),  to: TypeClass.Property
+  defdelegate defgenerator(name, do: body), to: TypeClass.Property.Generator
 end
