54de80b1d87c9213660650e4fb4b155fa6e6c641
Brooklyn Zelenka <bez@brooklynzelenka.com>
2016-10-24 02:54:17 -0700

    Update Haskell example in readme

--- README.md
+++ README.md
@@ -64,12 +64,11 @@ module Algebra.Monoid where
 class (Setoid a, Semigroup a) => Monoid a where
   identity :: a -> a
 
-  -- minimally complete & aliases
-  (^) :: a -> a
-  (^) = identity
-
-append_id :: a -> a
-append_id a = identity a `append` a
+  -- minimally complete definition
+  append_id :: a -> a
+  append_id a = identity a `append` a
+  -- not actually needed in this case
+  -- just here for illustration
 
 instance Monoid [a] where
   identity _ = []
--- test/reference.hs
+++ test/reference.hs
@@ -3,12 +3,13 @@ module Algebra.Monoid where
 class (Setoid a, Semigroup a) => Monoid a where
   identity :: a -> a
 
-  -- minimally complete & aliases
-  (^) :: a -> a
-  (^) = identity
+  -- minimally complete definition
+  append_id :: a -> a
+  append_id a = identity a `append` a
+  -- not actually needed in this case
+  -- just here for illustration
 
-append_id :: a -> a
-append_id a = identity a `append` a
+
 
 instance Monoid [a] where
   identity _ = []
