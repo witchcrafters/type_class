529247d1b66c479b8c2f354ea77cddab47366035
Brooklyn Zelenka <be.zelenka@gmail.com>
2018-06-15 12:36:41 -0700

    Format defalias

--- .formatter.exs
+++ .formatter.exs
@@ -2,12 +2,14 @@
 [
   inputs: ["mix.exs", "{config,lib,test}/**/*.{ex,exs}"],
   locals_without_parens: [
+    defalias: 2,
     defclass: 2,
     definst: :*,
     extend: :*
   ],
   export: [
     locals_without_parens: [
+      defalias: 2,
       defclass: 2,
       definst: :*,
       extend: :*
